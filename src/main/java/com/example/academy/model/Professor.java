fb377aab34cc32185487e24b701c517c1b54b9b6
kellyxwz <kellyramosdeoliveira25@gmail.com>
2026-07-22 19:03:24 -0300

    alterando o banco de dados para o postgresql

--- src/main/java/com/example/academy/model/Professor.java
+++ src/main/java/com/example/academy/model/Professor.java
@@ -3,7 +3,7 @@ package com.example.academy.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "professores")
+
 public class Professor {
 
     @Id
