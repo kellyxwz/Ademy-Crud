864c5ba2abd82d7893bedb8d1fb2fa09066f3281
kellyxwz <kellyramosdeoliveira25@gmail.com>
2026-07-21 23:11:22 -0300

    criado o repositorio de Disciplina

---
+++ src/main/java/com/example/academy/repository/DisciplinaRepository.java
@@ -0,0 +1,10 @@
+package com.example.academy.repository;
+
+import com.example.academy.model.Disciplina;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface DisciplinaRepository extends MongoRepository<Disciplina, String> {
+
+    git
+
+}
