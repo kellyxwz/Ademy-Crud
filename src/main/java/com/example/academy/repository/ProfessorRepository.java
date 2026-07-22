041ca386533b640fdac322124bd3e475c5a417f0
kellyxwz <kellyramosdeoliveira25@gmail.com>
2026-07-22 19:15:00 -0300

    alterando o tipo de variavel do parametro do id no ProfessorRepository

--- src/main/java/com/example/academy/repository/ProfessorRepository.java
+++ src/main/java/com/example/academy/repository/ProfessorRepository.java
@@ -3,6 +3,6 @@ package com.example.academy.repository;
 import com.example.academy.model.Professor;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ProfessorRepository extends MongoRepository<Professor, String> {
+public interface ProfessorRepository extends MongoRepository<Professor, Long> {
 
 }
