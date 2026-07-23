818e65d319884f5f7f8b82be8ae00f1782f5108d
kellyxwz <kellyramosdeoliveira25@gmail.com>
2026-07-23 09:02:39 -0300

    alterando os métodos do ProfessorController

--- src/main/java/com/example/academy/controller/ProfessorController.java
+++ src/main/java/com/example/academy/controller/ProfessorController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/professores")
+@RequestMapping("/api/professores")
 public class ProfessorController {
 
     private final ProfessorService service;
