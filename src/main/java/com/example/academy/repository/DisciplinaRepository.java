d94780998ccc04eb5e7a42968294444c523bee99
kellyxwz <kellyramosdeoliveira25@gmail.com>
2026-07-24 08:00:20 -0300

    adicionado a interface JpaSpecifiatonExecutor nos repositorios de professor e disciplina

--- src/main/java/com/example/academy/repository/DisciplinaRepository.java
+++ src/main/java/com/example/academy/repository/DisciplinaRepository.java
@@ -2,7 +2,8 @@ package com.example.academy.repository;
 
 import com.example.academy.model.Disciplina;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> {
+public interface DisciplinaRepository extends JpaRepository<Disciplina, Long>, JpaSpecificationExecutor<Disciplina> {
 
 }
--- src/main/java/com/example/academy/repository/ProfessorRepository.java
+++ src/main/java/com/example/academy/repository/ProfessorRepository.java
@@ -2,7 +2,8 @@ package com.example.academy.repository;
 
 import com.example.academy.model.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+public interface ProfessorRepository extends JpaRepository<Professor, Long>, JpaSpecificationExecutor<Professor> {
 
 }
