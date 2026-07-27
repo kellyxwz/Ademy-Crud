436c1cef093cca1cae31399f4b513358417556b9
kellyxwz <kellyramosdeoliveira25@gmail.com>
2026-07-26 21:20:40 -0300

    criando a classe SecurityFilter para controlar a autorização dos acessos as rotas

--- src/main/java/com/example/academy/config/JwtFilter.java
+++ src/main/java/com/example/academy/config/JwtFilter.java
@@ -27,20 +27,25 @@ public class JwtFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
+
         final String authHeader = request.getHeader("Authorization");
         String username =null;
         String jwt=null;
 
-        if(authHeader != null && authHeader.startsWith("Barear")){
+        if(authHeader != null && authHeader.startsWith("Bearer")){
             jwt = authHeader.substring(7);
             username = jwtUtil.extractUsername(jwt);
         }
 
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null){
             UserDetails userDetails = service.loadUserByUsername(username);
+
             if (jwtUtil.isTokenValid(jwt, userDetails.getUsername())){
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,
-                        null, userDetails.getAuthorities());
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities());
+
                 authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
---
+++ src/main/java/com/example/academy/config/SecurityFilter.java
@@ -0,0 +1,59 @@
+package com.example.academy.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityFilter {
+
+    private final JwtFilter jwt;
+
+    public SecurityFilter(JwtFilter jwt) {
+        this.jwt = jwt;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http ) throws Exception{
+        http.csrf( csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests( auth ->auth
+                        .requestMatchers("/api/auth/login").permitAll()
+                        .requestMatchers("/api/auth/register").hasRole("PROFESSOR")
+                        .requestMatchers(HttpMethod.GET, "/**").hasAnyRole("ALUNO", "PROFESSOR")
+                        // apenas PROFESSOR pode criar, editar e excluir
+                        .requestMatchers(HttpMethod.POST, "/**").hasRole("PROFESSOR")
+                        .requestMatchers(HttpMethod.PUT, "/**").hasRole("PROFESSOR")
+                        .requestMatchers(HttpMethod.DELETE, "/**").hasRole("PROFESSOR")
+                        .anyRequest().hasRole("PROFESSOR")
+                )
+                .addFilterBefore(jwt, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public FilterRegistrationBean<JwtFilter> jwtFilterFilterRegistration (JwtFilter jwt){
+        FilterRegistrationBean<JwtFilter> registration = new FilterRegistrationBean<>();
+        registration.setEnabled(false);
+
+        return registration;
+
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
+
+}
