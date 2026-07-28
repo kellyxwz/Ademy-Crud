21b2995483091d0ff31f24bff5c492c10ceeedb0
kellyxwz <kellyramosdeoliveira25@gmail.com>
2026-07-28 17:21:11 -0300

    adicionando regras de validação no meu requestdto de disciplina

--- src/main/java/com/example/academy/dto/request/DisciplinaRequestDTO.java
+++ src/main/java/com/example/academy/dto/request/DisciplinaRequestDTO.java
@@ -1,7 +1,17 @@
 package com.example.academy.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record DisciplinaRequestDTO(
+
+        @NotBlank(message = "O nome é obrigatório")
+        @Size(min = 3, max = 120, message = "O nome da disciplina deve ter entre 3 a 120 caracteres")
         String nome,
+
+        @NotBlank(message = "A carga horária é obrigatória")
+        @Min(value = 1, message = "A carga horária deve ser maior q 1")
         Integer cargaHoraria
 ) {
 }
