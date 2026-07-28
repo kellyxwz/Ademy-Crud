320db7f6b71714b58caa90c4e6e5676b76fb1772
kellyxwz <kellyramosdeoliveira25@gmail.com>
2026-07-28 17:30:36 -0300

    adicionando regras de nas colunas da entidade Usuário

--- src/main/java/com/example/academy/model/Usuario.java
+++ src/main/java/com/example/academy/model/Usuario.java
@@ -2,7 +2,9 @@ package com.example.academy.model;
 
 
 import jakarta.persistence.*;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "usuario")
@@ -12,14 +14,19 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Username é obrigatória")
     @Column(nullable = false, unique = true)
     private String username;
 
+    @NotBlank(message = "Senha é obrigatória")
     @Column(nullable = false)
+    @Size(min = 6, max = 20, message = "A senha deve ter entre 6 à 20 caracteres")
     private String password;
 
+    @NotBlank(message = "Role é obrigatória")
+    @Pattern(regexp = "^(ALUNO|PROFESSOR)$", message = "Role deve ser ALUNO ou PROFESSOR")
+    @Column(nullable = false, length = 20)
     private String role;
-
     public Usuario() {
     }
 
