e4fad34a9fa2e667b6cc3823033d74d6f5e236e6
kellyxwz <kellyramosdeoliveira25@gmail.com>
2026-07-28 17:15:48 -0300

    adicionando regras de validação no meu requestdto de professor

--- src/main/java/com/example/academy/dto/request/ProfessorRequestDTO.java
+++ src/main/java/com/example/academy/dto/request/ProfessorRequestDTO.java
@@ -1,8 +1,21 @@
 package com.example.academy.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record ProfessorRequestDTO(
+
+        @NotBlank(message = "O nome é obrigatório")
+        @Size(min = 2, max = 120, message = "O nome deve ter entre 2 e 120 caracteres")
         String nome,
+
+        @NotBlank(message = "A especialidade é obrigatória")
+        @Size(min = 2, max = 50, message = "A especialidade deve ter entre 2 e 50 caracteres")
         String especialidade,
+
+        @NotBlank(message = "O e-mail é obrigatório")
+        @Email(message = "E-mail inválido")
         String email
 ) {
 }
